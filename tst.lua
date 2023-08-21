7ddf7fa4e7179a9f73fd970c1e22ecdd8d5e0789
Austin Michael Larsen <austin.larsen@mailfence.com>
2023-08-20 22:22:57 -0400

    synth with lua

--- tst.lua
+++ tst.lua
@@ -1,3 +1,7 @@
 local audio = require("audio")
-audio.playOscillator("sine", 80, 0.5)
-while true do end
+audio.playOscillator("square", 80, 0.5)
+
+while true do
+    local s = io.read(1)
+    print(s)
+end

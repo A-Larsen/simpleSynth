b385670c325ae28778148af9f2209c9b9050741d
Austin Michael Larsen <austin.larsen@mailfence.com>
2023-08-21 16:12:10 -0400

    mixer | master amp

--- lualib.c
+++ lualib.c
@@ -14,21 +14,19 @@
 bool AUDIO_START = false;
 Mixer mixer;
 
-uint8_t OSCILLATOR_ID = 1;
-uint8_t WAVEFILE_ID = 1;
-
 __declspec(dllexport) int loadWav(lua_State *L)
 {
+    static uint8_t WAVEFILE_COUNT = 1;
     const char *file_path = (const char *)luaL_checkstring(L, 1);
     WavPlayer *userdata = (WavPlayer *)lua_newuserdata(L,
                                  sizeof(WavPlayer));
     char name[50];
-    sprintf(name, "%s_%d", "WAVE_FILE", WAVEFILE_ID);
+    sprintf(name, "%s_%d", "WAVE_FILE", WAVEFILE_COUNT);
     lua_setglobal(L, name);
 
     WavPlayer_init(userdata, file_path, L);
-    lua_pushnumber(L, (lua_Number)WAVEFILE_ID);
-    WAVEFILE_ID++;
+    lua_pushnumber(L, (lua_Number)WAVEFILE_COUNT);
+    WAVEFILE_COUNT++;
     return 1;
 }
 __declspec(dllexport) int playWav(lua_State *L)
@@ -55,6 +53,7 @@ __declspec(dllexport) int playOscillator(lua_State *L)
 
 __declspec(dllexport) int loadOsillator(lua_State *L)
 {
+    static uint8_t OSCILLATOR_COUNT = 1;
     char *osc = (char *)luaL_checkstring(L,  1);
     float freq = (float)luaL_checknumber(L,  2);
     float amp = (float)luaL_checknumber(L,  3);
@@ -62,7 +61,7 @@ __declspec(dllexport) int loadOsillator(lua_State *L)
     BasicOscillator *userdata = (BasicOscillator *)lua_newuserdata(L,
                                  sizeof(BasicOscillator));
     char name[50];
-    sprintf(name, "%s_%d", "BASIC_OSCILLATOR", OSCILLATOR_ID);
+    sprintf(name, "%s_%d", "BASIC_OSCILLATOR", OSCILLATOR_COUNT);
     lua_setglobal(L, name);
 
 
@@ -78,10 +77,11 @@ __declspec(dllexport) int loadOsillator(lua_State *L)
         BasicOscillator_setType(userdata, OSCILLATOR_SQUARE);
     }
 
-    lua_pushnumber(L, (lua_Number)OSCILLATOR_ID);
-    OSCILLATOR_ID++;
+    lua_pushnumber(L, (lua_Number)OSCILLATOR_COUNT);
+    OSCILLATOR_COUNT++;
     return 1;
 }
+
 __declspec(dllexport) int masterAmp(lua_State *L)
 {
     mixer.master_amp = (float)luaL_checknumber(L, 1);

c3a26b59bad55f8a12957be3a328a3396e26a100
Austin Michael Larsen <austin.larsen@mailfence.com>
2023-08-21 16:17:40 -0400

    refactoring

--- synth.c
+++ synth.c
@@ -23,21 +23,37 @@ int main(int argc, char **argv)
         BasicOscillator_mapKey(&userdata, key);
 
 		switch(key) {
-			case 27: { // esc
-				quit = true;
-                break;
-			} 
-			case '1': BasicOscillator_setType(&userdata, OSCILLATOR_SINE); break;
-			case '2': BasicOscillator_setType(&userdata, OSCILLATOR_SAW); break;
-			case '3': BasicOscillator_setType(&userdata, OSCILLATOR_SQUARE); break;
-            case '-': BasicOscillator_ampDown(&userdata); break;
-            case '=': BasicOscillator_ampUp(&userdata); break;
-            case 'z': BasicOscillator_setBasePitch(&userdata,
-                                                   userdata.base_pitch - 12);
-                      break;
-            case 'x': BasicOscillator_setBasePitch(&userdata,
-                                                   userdata.base_pitch + 12); 
-                      break;
+        case 27:  // esc
+            quit = true;
+            break;
+        
+        case '1': 
+            BasicOscillator_setType(&userdata, OSCILLATOR_SINE); 
+            break;
+
+        case '2':
+            BasicOscillator_setType(&userdata, OSCILLATOR_SAW); 
+            break;
+
+        case '3': 
+            BasicOscillator_setType(&userdata, OSCILLATOR_SQUARE);
+            break;
+
+        case '-': 
+            BasicOscillator_ampDown(&userdata);
+            break;
+
+        case '=': 
+            BasicOscillator_ampUp(&userdata);
+            break;
+
+        case 'z': 
+            BasicOscillator_setBasePitch(&userdata, userdata.base_pitch - 12);
+            break;
+
+        case 'x': 
+            BasicOscillator_setBasePitch(&userdata, userdata.base_pitch + 12); 
+            break;
 		}
 	}
 

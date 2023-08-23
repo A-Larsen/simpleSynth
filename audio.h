292f4e02657ab40f907c389c092c9b1f5ee7229e
Austin Michael Larsen <austin.larsen@mailfence.com>
2023-08-23 11:29:04 -0400

    can play multiple things at once

--- audio.h
+++ audio.h
@@ -39,11 +39,6 @@ int wav_init(WavData *wavdata, void (*initStream) (struct _WavData *wavdata),
             void (*handleStream) (int16_t *stream, struct _WavData *wavdata),
             WAVEFORMATEX *format, void *data) 
 {
-    /* wavdata->format.wFormatTag = WAVE_FORMAT_PCM; */
-    /* wavdata->format.nChannels = 1; */
-    /* wavdata->format.nSamplesPerSec = SAMPLING_RATE; */
-    /* wavdata->format.wBitsPerSample = 16; */
-    /* wavdata->format.cbSize = 0; */
     wavdata->chunk_swap = 0;
     wavdata->data = data;
     wavdata->initStream = initStream;
@@ -51,11 +46,13 @@ int wav_init(WavData *wavdata, void (*initStream) (struct _WavData *wavdata),
     wavdata->play = false;
 
     WAVEHDR *header = wavdata->header;
-    /* WAVEFORMATEX *format = &wavdata->format; */
 
     format->nBlockAlign = format->nChannels * format->wBitsPerSample / 8;
     format->nAvgBytesPerSec = format->nSamplesPerSec * format->nBlockAlign;
 
+    // WAVE_MAPPER automatically selects the best device
+    // would probably be a good idea to have the option to actuall selec the
+    // device in the future
     if(waveOutOpen(&wavdata->wave_out, WAVE_MAPPER, format, (DWORD_PTR)WaveOutProc,
                 (DWORD_PTR)wavdata, CALLBACK_FUNCTION) != MMSYSERR_NOERROR) {
         PERROR("waveOutOpen failed\n");
@@ -70,7 +67,6 @@ int wav_init(WavData *wavdata, void (*initStream) (struct _WavData *wavdata),
 
     // you don't need to process the audio twice, you just need to initialize
     // your data and send it to waveOutPrepareHeader and waveOutWrite
-	/* wavdata.wave_step = TWOPI / ((float)SAMPLING_RATE / wavdata.frequency); */
     memset(header, 0, sizeof(WAVEHDR) * CHUNK_COUNT);
     memset(&wavdata->chunks, 0, sizeof(int16_t) * CHUNK_SIZE * CHUNK_COUNT);
 
@@ -103,14 +99,9 @@ void CALLBACK WaveOutProc(HWAVEOUT wave_out_handle, UINT message,
             break;
         }
         case WOM_OPEN:  {
-            // Give windows time to prepare audio
-            /* printf("Prepareing..\n"); */
-            /* Sleep(1500); */
-            /* printf("ready\n"); */
             break;
         }
 		case WOM_DONE:{ 
-            /* while(AUDIO_START == false); */
             if(!wavdata->play || !AUDIO_START) break;
 			for(int i = 0; i < CHUNK_SIZE; ++i) {
                 wavdata->handleStream(&wavdata->chunks[wavdata->chunk_swap][i], wavdata);
--- tst.lua
+++ tst.lua
@@ -3,10 +3,14 @@ local audio = require("audio")
 audio.masterAmp(.3)
 
 local osc_1 = audio.loadOsillator("square", 40, 0.5)
+local osc_2 = audio.loadOsillator("square", 60, 0.5)
 local wav_1 = audio.loadWav("seaShells.wav")
+local wav_2 = audio.loadWav("i_win.wav")
 
 audio.playWav(wav_1)
+audio.playWav(wav_2)
 audio.playOscillator(osc_1)
+audio.playOscillator(osc_2)
 audio.play()
 
 while true do
